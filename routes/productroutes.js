3e6b65b468f7f564bf94f195562f0f5287af4159
advika sharmna <sharmaadvikaa@gmail.com>
2024-08-06 18:01:38 +0530

    male fashion continued

---
+++ routes/productroutes.js
@@ -0,0 +1,6 @@
+const getproductcontroller = require("../controllers/products/getproductcontroller")
+
+const express = require("express")
+const router = express.Router()
+router.get("/",getproductcontroller)
+module.exports = router
