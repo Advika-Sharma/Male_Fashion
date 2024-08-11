c1f27bdf0f530dab371f236e29c93d773efd14e6
advika sharmna <sharmaadvikaa@gmail.com>
2024-08-11 15:17:10 +0530

    movies api project

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-const express = require("express")
-const color = require("colors")
-const dotenv = require("dotenv").config()
-const morgan = require("morgan")
-const rootroute = require('./routes/rootroute')
-const connectDB = require("./config/db")
-const productroute=require("./routes/productroutes")
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const rootroute = require("./routes/rootroute");
+const movieroute = require("./routes/movieroute");
+const connectDB = require("./config/db");
 
-const app =express()
-const PORT = process.env.PORT || 7000 || 8000 
+const PORT = process.env.PORT || 8000 || 6000;
+const app = express();
 
-app.use('/',rootroute)
-connectDB()
-app.use('/products',productroute)
-app.listen(PORT,()=>{
-    console.log(PORT);
-    console.log(`Server is running on ${PORT}`.bgBlue.white)
-})
+connectDB();
+
+app.use("/", rootroute);
+app.use("/movies", movieroute);
+
+// server create
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`.bgBlue.green);
+});
